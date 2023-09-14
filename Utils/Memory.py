fd5f5d71565a90512d6eded116a136722f160fcd
SwaggyMacro <macro@julym.com>
2023-09-14 21:56:33 +0800

    Improves the way to get the token and UUID.

--- Utils/Memory.py
+++ Utils/Memory.py
@@ -1,5 +1,4 @@
-import ctypes
-import re
+import json
 
 import pymem
 import win32gui
@@ -46,44 +45,31 @@ def get_token_uuid_new():
         try:
             pm = pymem.Pymem(pid)
             # bytes_pattern = r'&env=production#sid='.encode('utf-8')
-            bytes_pattern = r'.home-cards.d0'.encode('utf-8')
+            bytes_pattern = r'requestHeaders'.encode('utf-8')
             character_count_address = pymem.pattern.pattern_scan_all(pm.process_handle, bytes_pattern,
                                                                      return_multiple=True)
             # print(character_count_address)
             for address in character_count_address:
                 try:
-                    text = pm.read_string(address, 300)
+                    text = pm.read_string(address, 600)
+                    text = text.replace(" ", '').replace('\\', '')[text.find('requestHeaders":"') +
+                                                                   17:text.find('","responseHeaders')]
+                    # find the last }" and remove the rest
+                    text = text[:text.rfind('}') + 1]
+
+                    # print(text)
+
+                    req_headers = json.loads(text)
+
+                    # print(req_headers)
+
+                    if "csr-uuid" in req_headers.keys() and "csr-token" in req_headers.keys():
+                        uuid = req_headers["csr-uuid"]
+                        token = req_headers["csr-token"]
+                        break
+
                 except:
                     continue
-                uuid_pattern = r'uuid=([0-9a-fA-F-]+)'
-                sid_pattern = r'sid=([0-9a-fA-F-]+)'
-
-                uuid_match = re.search(uuid_pattern, text)
-                sid_match = re.search(sid_pattern, text)
-
-                if uuid_match and uuid is None:
-                    uuid = uuid_match.group(1)
-
-                if sid_match and token is None:
-                    token = sid_match.group(1)
-
-            # print('----------------')
-
-            # bytes_pattern = '&uuid='.encode('utf-8')
-            # character_count_address = pymem.pattern.pattern_scan_all(pm.process_handle, bytes_pattern,
-            #                                                          return_multiple=True)
-            # # print(character_count_address)
-            # try:
-            #     for address in character_count_address:
-            #         uuid = pm.read_string(address, 60)
-            #         # 正则表达式 匹配
-            #         pattern = r'[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}'
-            #         uuids = re.findall(pattern, uuid)
-            #         if len(uuids) > 0:
-            #             uuid = uuids[0]
-            #             break
-            # except:
-            #     uuid = None
 
             return token is not None and token.strip() or None, uuid is not None and uuid.strip() or None
 
